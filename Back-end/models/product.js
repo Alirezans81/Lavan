ff5e70afa5814b54083302cc441b6a878d6b9399
Alireza <alireza.nasiri.381@gmial.com>
2022-07-29 20:40:36 +0430

    products api done

--- Back-end/models/product.js
+++ Back-end/models/product.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const countSchema = mongoose.Schema({
-  purchased: {type: Boolean, default: false, required: true}
+  purchased: { type: Boolean, default: false, required: true },
 });
 
 const sizeSchema = mongoose.Schema({
-  size: {type: Number, required: true},
-  count: {type: [countSchema], required: true, default: []}
+  size: { type: Number, required: true },
+  count: { type: [countSchema], required: true, default: [] },
 });
 
 const colorSchema = mongoose.Schema({
-  color_name: {type: String, required: true},
-  sizes: {type: [sizeSchema], required: true, default: []}
+  color_name: { type: String, required: true },
+  sizes: { type: [sizeSchema], required: true, default: [] },
 });
 
 const productSchema = mongoose.Schema({
   imgSrc: {type: String, required: true},
-  colors: {type: [colorSchema], required: true, default: []}
+  title: { type: String, required: true },
+  desc: String,
+  colors: { type: [colorSchema], required: true, default: [] },
 });
 
 const Product = mongoose.model("Product", productSchema);
--- Back-end/routes/products.js
+++ Back-end/routes/products.js
@@ -1,17 +1,121 @@
 // imports
 const express = require("express");
+const Product = require("../models/product");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
 // end of imports
 
-// intits
-const router = express.Router();
-// end of intits
+router.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.json({
+    data: products,
+    message: "ok",
+  });
+});
 
-app.get("/", (req, res) => {});
+router.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      data: null,
+      message: "product not found",
+    });
+  }
 
-app.get("/:id", (req, res) => {});
+  res.json({
+    data: product,
+    message: "ok",
+  });
+});
 
-app.get("/add", (req, res) => {});
+router.post(
+  "/",
+  [
+    body("imgSrc", "imgSrc cant be empty").isURL(),
+    body("imgSrc", "imgSrc must be URL").isURL(),
+    body("title", "title cant be empty").notEmpty(),
+    body("colors", "colors cant be empty").notEmpty(),
+    body("colors", "colors must be array").isArray(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        data: null,
+        errors: errors.array(),
+        message: "validation error",
+      });
+    }
 
-app.get("/remove", (req, res) => {});
+    let newProduct = new Product({
+      imgSrc: req.params.imgSrc,
+      title: req.params.title,
+      desc: req.params.desc,
+      colors: req.params.colors,
+    });
+
+    newProduct = await newProduct.save();
+
+    res.json({
+      data: newProduct,
+      message: "ok",
+    });
+  }
+);
+
+router.put(
+  "/:id",
+  [
+    body("imgSrc", "imgSrc cant be empty").isURL(),
+    body("imgSrc", "imgSrc must be URL").isURL(),
+    body("title", "title cant be empty").notEmpty(),
+    body("colors", "colors cant be empty").notEmpty(),
+    body("colors", "colors must be array").isArray(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        data: null,
+        errors: errors.array(),
+        message: "validation error",
+      });
+    }
+
+    const product = await Product.findByIdAndUpdate(req.params.id, {
+      imgSrc: req.params.imgSrc,
+      title: req.params.title,
+      desc: req.params.desc,
+      colors: req.params.colors,
+    }, {new: true});
+
+    if (!product) {
+      return res.status(404).json({
+        data: null,
+        message: "product not found",
+      });
+    }
+  
+    res.json({
+      data: product,
+      message: "ok",
+    });
+  }
+);
+
+router.delete("/:id", async (req, res) => {
+  const product = await Product.findByIdAndRemove(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      data: null,
+      message: "product not found",
+    });
+  }
+
+  res.json({
+    data: product,
+    message: "ok",
+  });
+});
 
 module.exports = router;
